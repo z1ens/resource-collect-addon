0f7070e79c4bfdacc101a236263f3f18b9945db2
z1ens <xxtale02591@gmail.com>
2024-06-14 09:20:30 +0200

    Initial commit

---
+++ pkg/addon/agent/calculate.go
@@ -0,0 +1,192 @@
+package agent
+
+import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/labels"
+	corev1informers "k8s.io/client-go/informers/core/v1"
+	corev1lister "k8s.io/client-go/listers/core/v1"
+	"k8s.io/klog/v2"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
+)
+
+// MAXSCORE Constants for scores
+const MAXSCORE = float64(100)
+const MINSCORE = float64(-100)
+
+// MAXCPUCOUNT Constants for resource counts
+const MAXCPUCOUNT = float64(100)
+const MINCPUCOUNT = float64(0)
+const MAXGPUCOUNT = float64(20) // Assume that one cluster can have maximum 10 GPUs, can be modified.
+const MINGPUCOUNT = float64(0)
+
+// MAXMEMCOUNT Constants for memory
+const MAXMEMCOUNT = float64(1024 * 1024)
+const MINMEMCOUNT = float64(0)
+
+// ResourceGPU Custom resource names
+const ResourceGPU = "nvidia.com/gpu"
+
+type Score struct {
+	nodeLister        corev1lister.NodeLister
+	useRequested      bool
+	enablePodOverhead bool
+	podLister         corev1lister.PodLister
+}
+
+func NewScore(nodeInformer corev1informers.NodeInformer, podInformer corev1informers.PodInformer) *Score {
+	return &Score{
+		nodeLister:        nodeInformer.Lister(),
+		podLister:         podInformer.Lister(),
+		enablePodOverhead: true,
+		useRequested:      true,
+	}
+}
+
+func (s *Score) calculateScore() (cpuScore int64, memScore int64, gpuScore int64, err error) {
+	cpuAlloc, err := s.calculateClusterAllocatable(string(clusterv1.ResourceCPU))
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	memAlloc, err := s.calculateClusterAllocatable(string(clusterv1.ResourceMemory))
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	gpuAlloc, err := s.calculateClusterAllocatable(ResourceGPU)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	cpuUsage, err := s.calculatePodResourceRequest(string(v1.ResourceCPU))
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	memUsage, err := s.calculatePodResourceRequest(string(v1.ResourceMemory))
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	gpuUsage, err := s.calculatePodResourceRequest(ResourceGPU)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	return s.normalizeScore(cpuAlloc, cpuUsage, memAlloc, memUsage, gpuAlloc, gpuUsage)
+}
+
+func (s *Score) normalizeScore(cpuAlloc, cpuUsage, memAlloc, memUsage, gpuAlloc, gpuUsage float64) (cpuScore int64, memScore int64, gpuScore int64, err error) {
+	klog.Infof("cpuAlloc = %v, cpuUsage = %v, memAlloc = %v, memUsage = %v, gpuAlloc = %v, gpuUsage = %v", cpuAlloc, cpuUsage, memAlloc, memUsage, gpuAlloc, gpuUsage)
+	availableCpu := cpuAlloc - cpuUsage
+	if availableCpu > MAXCPUCOUNT {
+		cpuScore = int64(MAXSCORE)
+	} else if availableCpu <= MINCPUCOUNT {
+		cpuScore = int64(MINSCORE)
+	} else {
+		cpuScore = int64(200*availableCpu/MAXCPUCOUNT - 100)
+	}
+
+	availableMem := (memAlloc - memUsage) / (1024 * 1024) // MB
+	if availableMem > MAXMEMCOUNT {
+		memScore = int64(MAXSCORE)
+	} else if availableMem <= MINMEMCOUNT {
+		memScore = int64(MINSCORE)
+	} else {
+		memScore = int64(200*availableMem/MAXMEMCOUNT - 100)
+	}
+
+	availableGpu := gpuAlloc - gpuUsage
+	if availableGpu > MAXGPUCOUNT {
+		gpuScore = int64(MAXSCORE)
+	} else if availableGpu <= MINGPUCOUNT {
+		gpuScore = int64(MINSCORE)
+	} else {
+		gpuScore = int64(200*availableGpu/MAXGPUCOUNT - 100)
+	}
+
+	klog.Infof("cpuScore = %v, memScore = %v, gpuScore = %v", cpuScore, memScore, gpuScore)
+	return cpuScore, memScore, gpuScore, nil
+}
+
+func (s *Score) calculateClusterAllocatable(resourceName string) (float64, error) {
+	nodes, err := s.nodeLister.List(labels.Everything())
+	if err != nil {
+		return 0, err
+	}
+
+	allocatableList := make(map[string]resource.Quantity)
+	for _, node := range nodes {
+		if node.Spec.Unschedulable {
+			continue
+		}
+		for key, value := range node.Status.Allocatable {
+			if allocatable, exist := allocatableList[string(key)]; exist {
+				allocatable.Add(value)
+				allocatableList[string(key)] = allocatable
+			} else {
+				allocatableList[string(key)] = value
+			}
+		}
+	}
+	quantity, exists := allocatableList[resourceName]
+	if !exists {
+		return 0, nil
+	}
+	return quantity.AsApproximateFloat64(), nil
+}
+
+func (s *Score) calculatePodResourceRequest(resourceName string) (float64, error) {
+	list, err := s.podLister.List(labels.Everything())
+	if err != nil {
+		return 0, err
+	}
+
+	var podRequest float64
+	for _, pod := range list {
+		for i := range pod.Spec.Containers {
+			container := &pod.Spec.Containers[i]
+			value := s.getRequestForResource(resourceName, &container.Resources.Requests, !s.useRequested)
+			podRequest += value
+		}
+
+		for i := range pod.Spec.InitContainers {
+			initContainer := &pod.Spec.InitContainers[i]
+			value := s.getRequestForResource(resourceName, &initContainer.Resources.Requests, !s.useRequested)
+			if podRequest < value {
+				podRequest = value
+			}
+		}
+
+		// If Overhead is being utilized, add to the total requests for the pod
+		if pod.Spec.Overhead != nil && s.enablePodOverhead {
+			if quantity, found := pod.Spec.Overhead[v1.ResourceName(resourceName)]; found {
+				podRequest += quantity.AsApproximateFloat64()
+			}
+		}
+	}
+	return podRequest, nil
+}
+
+func (s *Score) getRequestForResource(resource string, requests *v1.ResourceList, nonZero bool) float64 {
+	if requests == nil {
+		return 0
+	}
+	switch resource {
+	case string(v1.ResourceCPU):
+		// Override if un-set, but not if explicitly set to zero
+		if _, found := (*requests)[v1.ResourceCPU]; !found && nonZero {
+			return 100
+		}
+		return requests.Cpu().AsApproximateFloat64()
+	case string(v1.ResourceMemory):
+		// Override if un-set, but not if explicitly set to zero
+		if _, found := (*requests)[v1.ResourceMemory]; !found && nonZero {
+			return 200 * 1024 * 1024
+		}
+		return requests.Memory().AsApproximateFloat64()
+	default:
+		quantity, found := (*requests)[v1.ResourceName(resource)]
+		if !found {
+			return 0
+		}
+		return quantity.AsApproximateFloat64()
+	}
+}
---
+++ pkg/addon/agent/calculate_test.go
@@ -0,0 +1,125 @@
+package agent
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestNormalizeValue(t *testing.T) {
+	cases := []struct {
+		name           string
+		cpuAlloc       float64
+		cpuUsage       float64
+		memAlloc       float64
+		memUsage       float64
+		gpuAlloc       float64
+		gpuUsage       float64
+		expectCPUScore int64
+		expectMemScore int64
+		expectGPUScore int64
+	}{
+		{
+			name:           "usage < alloc",
+			cpuAlloc:       70,
+			cpuUsage:       30,
+			memAlloc:       1024 * 1024 * 1024 * 1024,
+			memUsage:       1024 * 1024 * 1024 * 500,
+			gpuAlloc:       8,
+			gpuUsage:       4,
+			expectCPUScore: -20,
+			expectMemScore: 2,
+			expectGPUScore: -60,
+		},
+		{
+			name:           "usage = alloc",
+			cpuAlloc:       70,
+			cpuUsage:       70,
+			memAlloc:       1024 * 1024 * 1024,
+			memUsage:       1024 * 1024 * 1024,
+			gpuAlloc:       8,
+			gpuUsage:       8,
+			expectCPUScore: -100,
+			expectMemScore: -100,
+			expectGPUScore: -100,
+		},
+		{
+			name:           "usage > alloc",
+			cpuAlloc:       70,
+			cpuUsage:       80,
+			memAlloc:       1024 * 1024 * 1024 * 1024,
+			memUsage:       1024 * 1024 * 1024 * 1025,
+			gpuAlloc:       8,
+			gpuUsage:       10,
+			expectCPUScore: -100,
+			expectMemScore: -100,
+			expectGPUScore: -100,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			score := Score{}
+			cpuScore, memScore, gpuScore, err := score.normalizeScore(c.cpuAlloc, c.cpuUsage, c.memAlloc, c.memUsage, c.gpuAlloc, c.gpuUsage)
+			require.NoError(t, err)
+			assert.Equal(t, c.expectCPUScore, cpuScore)
+			assert.Equal(t, c.expectMemScore, memScore)
+			assert.Equal(t, c.expectGPUScore, gpuScore)
+		})
+	}
+}
+
+func TestCalculatePodResourceRequest(t *testing.T) {
+	testPod := &corev1.Pod{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name: "test",
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceCPU:                    resource.MustParse("500m"),
+							corev1.ResourceMemory:                 resource.MustParse("1Gi"),
+							corev1.ResourceName("nvidia.com/gpu"): resource.MustParse("1"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	clientset := fake.NewSimpleClientset()
+	informerFactory := informers.NewSharedInformerFactory(clientset, 0)
+	podInformer := informerFactory.Core().V1().Pods()
+	nodeInformer := informerFactory.Core().V1().Nodes()
+	podInformer.Informer().GetStore().Add(testPod)
+
+	s := NewScore(nodeInformer, podInformer)
+
+	cpuRequest, err := s.calculatePodResourceRequest(string(corev1.ResourceCPU))
+	require.NoError(t, err)
+
+	cpuExpected := 0.5
+	assert.Equal(t, cpuExpected, cpuRequest)
+
+	memoryRequest, err := s.calculatePodResourceRequest(string(corev1.ResourceMemory))
+	require.NoError(t, err)
+
+	memoryExpected := float64(1073741824) // 1GiB
+	assert.Equal(t, memoryExpected, memoryRequest)
+
+	gpuRequest, err := s.calculatePodResourceRequest(ResourceGPU)
+	require.NoError(t, err)
+
+	gpuExpected := float64(1)
+	assert.Equal(t, gpuExpected, gpuRequest)
+}
